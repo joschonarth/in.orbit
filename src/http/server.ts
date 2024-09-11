e85c8d731569ece2c0d042ed3848d544f57d4f50
joschonarth <joschonarth@gmail.com>
2024-09-11 11:28:12 -0300

    Update NLW Pocket Full-Stack

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('HTTP server running!')
-})
+  })
