7cb673f8f34d405f387f528f858ba66dbdbe1449
joschonarth <joschonarth@gmail.com>
2024-09-12 08:48:32 -0300

    Update NLW Pocket Full-Stack

---
+++ src/functions/get-week-pending-goals.ts
@@ -0,0 +1,24 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { goals } from '../db/schema'
+import { and, lte, sql } from 'drizzle-orm'
+
+dayjs.extend(weekOfYear)
+
+export function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+}
