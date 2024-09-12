3430096f94eafb675debff8dccc9400fdf592e74
joschonarth <joschonarth@gmail.com>
2024-09-12 10:12:31 -0300

    Update NLW Pocket Full-Stack

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,11 +6,18 @@ import {
 } from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
